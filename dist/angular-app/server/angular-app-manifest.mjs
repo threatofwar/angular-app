ce6c8d840c2621b1318915c8ecb146edeb600e4b
Adlil Yaacob <adlil.yaacob@gmail.com>
2025-01-03 08:29:43 +0000

    update env allocation

--- dist/angular-app/server/angular-app-manifest.mjs
+++ dist/angular-app/server/angular-app-manifest.mjs
@@ -6,8 +6,8 @@ export default {
   locale: undefined,
   routes: undefined,
   assets: {
-    'index.csr.html': {size: 2194, hash: '11ccd6b5279c3d168d539f6772f152999fd30b0e553f52d91c50367c99727ad8', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 1007, hash: '4995dc9f963e0526acbbd9053e514831d6e11b2011aa8689c91769e910986eef', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 2194, hash: '160c7f833e8f0c78fc664ba356877a288df05919cbdc82d8727f681dd0bbd6ee', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 1007, hash: 'eeeb193dff24e52d66f36d563d13be18d93252330416b08181fb13c07a1e080b', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
     'styles-IUMMJFUG.css': {size: 8799, hash: 'f23+PG+IqiE', text: () => import('./assets-chunks/styles-IUMMJFUG_css.mjs').then(m => m.default)}
   },
 };
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ export const ENV = {
   production: false,
   appVersion: '1.0.0', // Example app version for development
   featureXEnabled: true, // Feature flag
-  apiBaseUrl: 'api url', // Development API endpoint
+  apiBaseUrl: 'https://api.shibidi.my', // Development API endpoint
   loginEndpoint: '/login',
 };
