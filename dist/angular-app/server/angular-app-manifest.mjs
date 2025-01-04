4279b36aa220169c79f8803f786628bf29951fc1
Adlil Yaacob <adlil.yaacob@gmail.com>
2025-01-04 13:43:56 +0000

    add email verification

--- dist/angular-app/server/angular-app-manifest.mjs
+++ dist/angular-app/server/angular-app-manifest.mjs
@@ -6,8 +6,8 @@ export default {
   locale: undefined,
   routes: undefined,
   assets: {
-    'index.csr.html': {size: 2194, hash: 'e6578ee57eea92c4163199423524d0d86d6929a28339bd7e3a29a639bebc04c7', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 1007, hash: '363445834f07ddfa22e049d1df44363c945adbd91686bf1d4d0e459bca8a31ba', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 2194, hash: '3daa3fd4d0d213e46211b7fa7a7ab31f940dbe3e8d4033de5a086bf964514763', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 1007, hash: 'b33935801299182f5d7b61897bfefc53861da9cafd88e46db154bf15b67b3420', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
     'styles-3RNXJRVR.css': {size: 10735, hash: 'PcQfrS5NufE', text: () => import('./assets-chunks/styles-3RNXJRVR_css.mjs').then(m => m.default)}
   },
 };
